6727fe8e07e424c1900981dc29452361206c6cb1
EyadSwaity <iyad.alswaiti@external.stellantis.com>
2023-05-15 12:18:17 +0300

    fixed huggingface.py by adding labels in preprocess data.

--- huggingface/huggingface.py
+++ huggingface/huggingface.py
@@ -13,6 +13,7 @@ import torch
 import os
 from pathlib import Path
 import argparse
+
 # dataset1 = pd.read_csv(f"{Path(__file__).parent}/cairs_processed.csv")
 def get_args():
     parser = argparse.ArgumentParser("huggingface")
